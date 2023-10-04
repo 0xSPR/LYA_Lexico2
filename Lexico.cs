3175013977dd0790edec32343518e596b219df4f
Sergio <lb18590244@queretaro.tecnm.mx>
2023-10-04 15:30:32 -0500

    OpRelacional

--- Lexico.cs
+++ Lexico.cs
@@ -43,18 +43,22 @@ namespace LYA_Lexico2
                             estado = 1;
                         else if (char.IsDigit(c))
                             estado = 2;
-                        else if (c == '=') // Asignacion
-                            estado = 9;
+                        else if (c == '=')
+                            estado = 9; // Asignacion
                         else if (c == '!')
                             estado = 11;
-                        else if (c == '<' || c == '>' || c == '!') // OpFactor
-                            estado = 10;
-                        else if (c == '+' || c == '-') // OpTermino
-                            estado = 12;
+                        else if (c == '>' )
+                            estado = 19; // OpRelacional
+                        else if (c == '<')
+                            estado = 18;
+                        else if (c == '|')
+                            estado = 11;
+                        else if (c == '+' || c == '-')
+                            estado = 12; // OpTermino
                         else if (c == '*' || c == '%' || c == '/')
-                            estado = 14;
+                            estado = 14; // OpFactor
                         else if (c == '?')
-                            estado = 17;
+                            estado = 17; // OpTerneario
                         else
                             estado = 8;
                         break;
@@ -108,11 +112,11 @@ namespace LYA_Lexico2
                         break;
                     case 8:
                         setClasificacion(Tipos.Caracter);
-                        if (c == '|')
+                        if ( c == '&')
                             estado = 11;
                         else if (c == '*' || c == '/')
                             estado = 14;
-                        else if (c == '\"' || char.IsLetterOrDigit(c))
+                        else if (char.IsLetterOrDigit(c) || char.IsWhiteSpace(c))
                             estado = 16;
                         else
                             estado = F;
@@ -130,7 +134,10 @@ namespace LYA_Lexico2
                         break;
                     case 11:
                         setClasificacion(Tipos.OpLogico);
-                        estado = F;
+                        if(c == '=')
+                            estado = 10;
+                        else
+                            estado = F;
                         break;
                     case 12:
                         setClasificacion(Tipos.OpTermino);
@@ -156,16 +163,28 @@ namespace LYA_Lexico2
                         break;
                     case 16:
                         setClasificacion(Tipos.Cadena);
-                        if(c != '\"')
+                        if(char.IsLetterOrDigit(c) || char.IsWhiteSpace(c))
                             estado = 8;
                         else
                             estado = F;
                         break;
-                        case 17:
-                            setClasificacion(Tipos.OpTerneario);
+                    case 17:
+                        setClasificacion(Tipos.OpTerneario);
+                        estado = F;
+                        break;
+                    case 18:
+                        if(c == '>' || c == '=')
+                            estado = 10;
+                        else
                             estado = F;
-                            break;
-                        
+                        break;
+                    case 19:
+                        setClasificacion(Tipos.OpRelacional);
+                        if(c == '=')
+                            estado = 10;
+                        else
+                            estado = F;
+                        break;
                 }
                 if (estado >= 0)
                 {
